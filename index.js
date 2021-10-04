905e4a3c043678cb039e33795a6b0df98a6607e9
andrewhlu <alu@ucsb.edu>
2021-10-04 12:14:45 -0700

    Fix whitespace

--- index.js
+++ index.js
@@ -4,76 +4,74 @@ const modalBody = document.getElementById("modalBody");
 const modalEmail = document.getElementById("modalEmail");
 
 function submitEmail() {
-	// Get email from form
-	var email = emailField.value;
+    // Get email from form
+    var email = emailField.value;
 
-	if(email.match(/.+\@.+\..+/g) != null) {
-		// Hide warning div
-		$('#emailWarning').attr("hidden", true);
+    if (email.match(/.+\@.+\..+/g) != null) {
+        // Hide warning div
+        $('#emailWarning').attr("hidden", true);
 
-		// Show modal div, display waiting
-		$('#loadingIcon').removeAttr("hidden");
-		modalTitle.innerHTML = "Please Wait";
-		modalBody.innerHTML = modalEmail.value = "";
-		$('#myModal').modal('show');
+        // Show modal div, display waiting
+        $('#loadingIcon').removeAttr("hidden");
+        modalTitle.innerHTML = "Please Wait";
+        modalBody.innerHTML = modalEmail.value = "";
+        $('#myModal').modal('show');
 
-		// Send email address to mailing list endpoint
-		var urlstring = "https://nextjs.ucsbieee.org/api/mailing-list/add";
-                
-                var body = {
-                    email: email
-                };
+        // Send email address to mailing list endpoint
+        var urlstring = "https://nextjs.ucsbieee.org/api/mailing-list/add";
 
-		var settings = {
-		  "async": true,
-		  "crossDomain": true,
-		  "url": urlstring,
-		  "method": "POST",
-                  "body": new URLSearchParams(body).toString()
-		};
+        var body = {
+            email: email
+        };
 
-		$.ajax(settings).done(function (response) {
-			console.log(response);
-		  if(response.success) { 
-		  	// Valid, show success screen
-		  	$('#loadingIcon').attr("hidden", true);
-		  	modalTitle.innerHTML = "Success!";
-		  	modalBody.innerHTML = "Success! We will add you to the mailing list shortly!";
-		  	modalEmail.innerHTML = "Email: " + response.email;
-		  	emailField.value = "";
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": urlstring,
+            "method": "POST",
+            "body": new URLSearchParams(body).toString()
+        };
 
-		  	setTimeout(function() {
-		  		$('#myModal').modal('hide');
-        	},5000);
-		  }
-		});
-	}
-	else {
-		// Show warning div
-		$('#emailWarning').removeAttr("hidden");
-	}
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+            if (response.success) {
+                // Valid, show success screen
+                $('#loadingIcon').attr("hidden", true);
+                modalTitle.innerHTML = "Success!";
+                modalBody.innerHTML = "Success! We will add you to the mailing list shortly!";
+                modalEmail.innerHTML = "Email: " + response.email;
+                emailField.value = "";
+
+                setTimeout(function () {
+                    $('#myModal').modal('hide');
+                }, 5000);
+            }
+        });
+    } else {
+        // Show warning div
+        $('#emailWarning').removeAttr("hidden");
+    }
 }
 
 function appendDomain(domain) {
-	// Get email from form
-	var email = emailField.value;
+    // Get email from form
+    var email = emailField.value;
 
-	// Get part before @ symbol
-	var emailPrefix = email.substr(0, email.indexOf('@')); 
+    // Get part before @ symbol
+    var emailPrefix = email.substr(0, email.indexOf('@'));
 
-	if(emailPrefix == "") {
-		// There was no @ symbol, just append the email domain
-		var newEmail = email + "@" + domain;
-	}
-	else {
-		// There was an @ symbol, append email domain to emailPrefix
-		var newEmail = emailPrefix + "@" + domain;
-		
-	}
-	emailField.value = newEmail;
+    if (emailPrefix == "") {
+        // There was no @ symbol, just append the email domain
+        var newEmail = email + "@" + domain;
+    } else {
+        // There was an @ symbol, append email domain to emailPrefix
+        var newEmail = emailPrefix + "@" + domain;
+
+    }
+    emailField.value = newEmail;
 }
 
 function clearEmail() {
-	emailField.value = "";
-	emailField.focus();
+    emailField.value = "";
+    emailField.focus();
 }
